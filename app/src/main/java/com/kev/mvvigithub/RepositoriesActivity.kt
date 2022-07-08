fcbc7fbe1487ac522d44deda0347c836e3480eea
kev87ian <kelvinian87@gmail.com>
2022-07-08 12:38:57 +0300

    implemented viewmodel

--- app/src/main/java/com/kev/mvvigithub/RepositoriesActivity.kt
+++ app/src/main/java/com/kev/mvvigithub/RepositoriesActivity.kt
@@ -4,15 +4,13 @@ import android.app.ProgressDialog
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.kev.mvvigithub.adapter.RepositoryAdapter
 import com.kev.mvvigithub.model.RepositoryList
-import com.kev.mvvigithub.network.RetrofitInstance
-import com.kev.mvvigithub.network.RetrofitService
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.kev.mvvigithub.viewmodel.RepositoryActivityViewModel
 
 class RepositoriesActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
@@ -23,7 +21,23 @@ class RepositoriesActivity : AppCompatActivity() {
         setContentView(R.layout.activity_repositories)
 
         initRecyclerView()
-        fetchData()
+        livedataListener()
+    }
+
+
+    private fun livedataListener() {
+
+        val viewModel = ViewModelProviders.of(this).get(RepositoryActivityViewModel::class.java)
+        viewModel.getRepositoryLiveDataObserver().observe(this, Observer<RepositoryList> {
+            if (it != null) {
+                repositoryAdapter.setListData(it.items)
+                repositoryAdapter.notifyDataSetChanged()
+            } else {
+                Toast.makeText(baseContext, "Error fetching data", Toast.LENGTH_LONG).show()
+            }
+
+        })
+        viewModel.makeAPICall()
     }
 
     private fun initRecyclerView() {
@@ -32,32 +46,6 @@ class RepositoriesActivity : AppCompatActivity() {
         repositoryAdapter = RepositoryAdapter(this)
         recyclerView.adapter = repositoryAdapter
         mDialog = ProgressDialog(this)
-    }
-
-
-    private fun fetchData() {
-        mDialog.setMessage("Working...")
-        mDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER)
-        mDialog.isIndeterminate = true
-        mDialog.setCancelable(false)
-        mDialog.show()
-        val retrofitInstance = RetrofitInstance.getRetrofitInstance().create(RetrofitService::class.java)
-        val call = retrofitInstance.getDataFromAPI("newyork")
-        call.enqueue(object : Callback<RepositoryList?> {
-            override fun onResponse(call: Call<RepositoryList?>, response: Response<RepositoryList?>) {
-                if (response.isSuccessful){
-                    repositoryAdapter.setListData(response.body()?.items!!)
-                    repositoryAdapter.notifyDataSetChanged()
-                    mDialog.hide()
-
-                }
-            }
-
-            override fun onFailure(call: Call<RepositoryList?>, t: Throwable) {
-
-                Toast.makeText(baseContext, "Error + ${t.localizedMessage}", Toast.LENGTH_LONG).show()
-            }
-        })
     }
 
 
---
+++ app/src/main/java/com/kev/mvvigithub/viewmodel/RepositoryActivityViewModel.kt
@@ -0,0 +1,46 @@
+package com.kev.mvvigithub.viewmodel
+
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.kev.mvvigithub.model.RepositoryData
+import com.kev.mvvigithub.model.RepositoryList
+import com.kev.mvvigithub.network.RetrofitInstance
+import com.kev.mvvigithub.network.RetrofitService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class RepositoryActivityViewModel : ViewModel() {
+    lateinit var repositoryListData: MutableLiveData<RepositoryList>
+
+    init {
+        repositoryListData  = MutableLiveData()
+    }
+
+    fun getRepositoryLiveDataObserver() : MutableLiveData<RepositoryList>{
+        return repositoryListData
+    }
+
+    fun makeAPICall(){
+
+        val retrofitInstance = RetrofitInstance.getRetrofitInstance().create(RetrofitService::class.java)
+        val call = retrofitInstance.getDataFromAPI("newyork")
+        call.enqueue(object : Callback<RepositoryList?> {
+            override fun onResponse(call: Call<RepositoryList?>, response: Response<RepositoryList?>) {
+                if (response.isSuccessful){
+                    repositoryListData.postValue(response.body())
+                }
+
+                else{
+                    repositoryListData.postValue(null)
+                }
+            }
+
+            override fun onFailure(call: Call<RepositoryList?>, t: Throwable) {
+
+            repositoryListData.postValue(null)
+            }
+        })
+    }
+}
